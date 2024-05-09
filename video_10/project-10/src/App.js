3580d9d653df696e69bda528fb2f9a0d927e3576
Abdul Rehman <hy.abdulrahman2@gmail.com>
2024-05-09 11:14:43 +0500

    upload viedo_10 code

--- video_10/project-10/src/App.js
+++ video_10/project-10/src/App.js
@@ -1,16 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
-import Users from './Users';
-import NoJsx from './NoJsx';
+import Users from './Users.js';
 
 function App() {
+
+  function Apple(){
+    return(
+      <h1>apple</h1>
+    )
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
+        <Users />
+        {Apple()}
+        <Apple />
 
-<Users />
-<NoJsx />
+{/*         
+        
+        <Users />
+        
+        <Users />
+        
+        <Users />
+        
+        <Users /> */}
+        
       </header>
     </div>
   );
--- video_10/project-10/src/Users.js
+++ video_10/project-10/src/Users.js
@@ -3,11 +3,7 @@ import React from 'react'
 export default function Users() {
 
   return (
-    <b>
-
-    <div>Users, 2 + 8 * 7 = {2 +8 * 7} , its auto sum funtion,  </div>
-    <strong>This is JSX</strong>
-    </b>
+    <div>Users {2 +8 * 7}</div>
   )
 }
 
